b2f822536bae14d73d2a7636971afdc8d9e8d39c
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-08-20 21:04:39 -0300

    feat: test inside function

---
+++ tests/testthat/test-get_pols_from_aqs_NA_data.R
@@ -0,0 +1,16 @@
+test_that("download_senamhi_pol works with data and no-data cases!",{
+  cm_code <- 112194
+
+  # There is no CO data in this date range but there is
+  # PM25 data
+  start_date <- "10/08/2022"
+  end_date <- "19/08/2022"
+  pol_code <- c("N_CO", "N_PM25")
+
+  all_hours <- seq(as.POSIXct("2022-08-10 00:00", tz = "America/Lima"),
+                   as.POSIXct("2022-08-19 23:00", tz = "America/Lima"),
+                   by = "hour")
+
+  cm_co_pm25 <- get_pols_from_aqs(cm_code, pol_code, start_date, end_date)
+  expect_equal(nrow(cm_co_pm25), length(all_hours))
+})
---
+++ tests/testthat/test-senamhi_retrieve_NA.R
@@ -0,0 +1,15 @@
+test_that("senamhi_retrieve works when there is no data!", {
+  cm_code <- 112194
+
+  # There is no CO data in this date range
+  start_date <- "10/08/2022"
+  end_date <- "19/08/2022"
+  co_code <- "N_CO"
+
+  all_hours <- seq(as.POSIXct("2022-08-10 00:00", tz = "America/Lima"),
+                   as.POSIXct("2022-08-19 23:00", tz = "America/Lima"),
+                   by = "hour")
+
+  cm_co <- senamhi_retrieve(cm_code, co_code, start_date, end_date)
+  expect_equal(nrow(cm_co), length(all_hours))
+})
