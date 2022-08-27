081dd4bc253a8685b7749f282bb3eaadb67635f3
Mario Gavidia-Calderón <mgavidiac@gmail.com>
2022-08-27 19:48:23 -0300

    feat: dowload SPIM sounding data

--- R/download_airport_sounding.R
+++ R/download_airport_sounding.R
@@ -1,7 +1,6 @@
-download_airport_sounding <- function(date, hour = 12, airport = "SPIM"){
-  date <- "15/01/2019" # to del
-  hour <- 12 # to del
-  airport <- "SPIM" # to del
+download_airport_sounding <- function(date, hour = 12, airport = "SPIM",
+                                      region = "samer"){
+
   day <- substr(date, 1, 2)
   month <- substr(date, 4, 5)
   year <- substr(date, 7, 10)
@@ -12,7 +11,7 @@ download_airport_sounding <- function(date, hour = 12, airport = "SPIM"){
 
   sounding <- httr::GET(url,
                         query  = list(
-                          region = "samer",
+                          region = region,
                           TYPE = "TEXT:LIST",
                           YEAR = year,
                           MONTH = month,
@@ -26,7 +25,34 @@ download_airport_sounding <- function(date, hour = 12, airport = "SPIM"){
   sound_html_table <- XML::getNodeSet(sound_html, "//pre")
   sound_data_html <- sound_html_table[[1]]
   sound_text <- utils::capture.output(sound_data_html)
-  sound_data <- sound_text[6:(length(sound_text) - 1)]
-  #TODO: transform sound data into a data frame
-  return(sound_data)
+  sound_data <- sound_text[6:(length(sound_text)-1)]
+
+  pres <- substr(sound_data, 2, 7)
+  hght <- substr(sound_data, 8, 14)
+  temp <- substr(sound_data, 15, 21)
+  dwpt <- substr(sound_data, 22, 28)
+  relh <- substr(sound_data, 29, 35)
+  mixr <- substr(sound_data, 36, 42)
+  drct <- substr(sound_data, 43, 49)
+  sknt <- substr(sound_data, 50, 56)
+  thta <- substr(sound_data, 57, 63)
+  thte <- substr(sound_data, 64, 70)
+  thtv <- substr(sound_data, 71, 77)
+
+  sound_df <- data.frame(
+    pres = pres,
+    hght = hght,
+    temp = temp,
+    dwpt = dwpt,
+    relh = relh,
+    mixr = mixr,
+    drct = drct,
+    sknt = sknt,
+    thta = thta,
+    thte = thte,
+    thtv = thtv
+  )
+
+  sound_df[, 1:11] <- sapply(sound_df[,1:11], as.numeric)
+  return(sound_df)
 }
